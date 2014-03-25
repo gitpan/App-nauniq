35e7d51ba975ec47adc31cdee89157a826eedfa4
Steven Haryanto <stevenharyanto@gmail.com>
2014-03-25 15:51:44 +0000

    Import of SHARYANTO/App-nauniq-0.04 from CPAN.
    gitpan-cpan-distribution: App-nauniq
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         SHARYANTO/App-nauniq-0.04.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

--- lib/App/nauniq.pm
+++ lib/App/nauniq.pm
@@ -1,6 +1,6 @@
 package App::nauniq;
 
-our $VERSION = '0.03'; # VERSION
+our $VERSION = '0.04'; # VERSION
 
 1;
 # ABSTRACT: Non-adjacent uniq
@@ -17,7 +17,7 @@ App::nauniq - Non-adjacent uniq
 
 =head1 VERSION
 
-version 0.03
+version 0.04
 
 =head1 SYNOPSIS
 
